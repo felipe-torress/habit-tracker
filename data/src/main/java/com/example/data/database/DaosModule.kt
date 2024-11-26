8e1cc213c2df0f91a0e97de5be9c4184b7cc0936
Felipe Torres <fetl9239@gmail.com>
2024-11-26 20:43:43 -0300

    feat: add hilt modules

---
+++ data/src/main/java/com/example/data/database/DaosModule.kt
@@ -0,0 +1,28 @@
+package com.example.data.database
+
+import com.example.data.database.dao.GoalDao
+import com.example.data.database.dao.HabitDao
+import com.example.data.database.dao.HabitTaskDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DaosModule {
+    @Provides
+    fun providesGoalDao(
+        database: HabitTrackerDatabase
+    ): GoalDao = database.goalDao()
+
+    @Provides
+    fun providesHabitDao(
+        database: HabitTrackerDatabase
+    ): HabitDao = database.habitDao()
+
+    @Provides
+    fun providesHabitTaskDao(
+        database: HabitTrackerDatabase
+    ): HabitTaskDao = database.habitTaskDao()
+}
