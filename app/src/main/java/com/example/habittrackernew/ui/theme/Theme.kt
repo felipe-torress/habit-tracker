b3d789f917dbeb66be10ebddc56d8020b7c97cf6
Felipe Torres <fetl9239@gmail.com>
2024-11-26 00:31:10 -0300

    feat: update colorScheme

--- app/src/main/java/com/example/habittrackernew/ui/theme/Theme.kt
+++ app/src/main/java/com/example/habittrackernew/ui/theme/Theme.kt
@@ -1,7 +1,20 @@
 package com.example.habittrackernew.ui.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
+val colorScheme = lightColorScheme(
+    primary = HabitTrackerColors.green500,
+    onPrimary = HabitTrackerColors.backgroundColor,
+    secondary = HabitTrackerColors.blue500,
+    onSecondary = HabitTrackerColors.backgroundColor,
+    background = HabitTrackerColors.backgroundColor,
+    onBackground = HabitTrackerColors.textColor,
+)
+
 @Composable
-fun HabitTrackerTheme(content: @Composable () -> Unit) = MaterialTheme(content = content)
+fun HabitTrackerTheme(content: @Composable () -> Unit) = MaterialTheme(
+    colorScheme = colorScheme,
+    content = content
+)
