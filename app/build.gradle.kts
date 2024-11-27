f1f27ffcf1a10095b0b1e612911cce37ada99ff9
Felipe Torres <fetl9239@gmail.com>
2024-11-26 21:29:53 -0300

    feat: add data dependency to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ kapt {
 
 dependencies {
 
+    implementation(project(":data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
