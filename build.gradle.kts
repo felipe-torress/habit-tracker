ce59d036fe3d7eb3bf983b8899e37e93ec4e6ec8
Felipe Torres <fetl9239@gmail.com>
2024-11-26 22:02:40 -0300

    feat: setup ktlintCheck

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,10 @@ plugins {
     alias(libs.plugins.dagger.hilt) apply false
     alias(libs.plugins.android.library) apply false
 }
+
+tasks.register("ktlintCheck") {
+    dependsOn(
+        ":app:ktlintCheck",
+        ":data:ktlintCheck"
+    )
+}
