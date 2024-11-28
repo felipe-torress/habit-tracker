e81962e88e545964fe98537342fc2c1132cc3db7
Felipe Torres <fetl9239@gmail.com>
2024-11-27 21:49:47 -0300

    feat: add append methods to TestTagState

--- app/src/main/java/com/example/habittrackernew/ui/utils/testTags/TestTagState.kt
+++ app/src/main/java/com/example/habittrackernew/ui/utils/testTags/TestTagState.kt
@@ -9,12 +9,17 @@ data class TestTagState(
     val index: String = "",
 ) {
     fun type(text: String) = this.copy(type = text)
+    fun appendToType(text: String) = this.copy(type = this.type + text)
 
     fun action(text: String) = this.copy(action = text)
+    fun appendToAction(text: String) = this.copy(action = this.action + text)
 
     fun section(text: String) = this.copy(section = text)
+    fun appendToSection(text: String) = this.copy(section = this.section + text)
 
     fun item(text: String) = this.copy(item = text)
+    fun appendToItem(text: String) = this.copy(item = this.item + text)
 
     fun index(text: String) = this.copy(index = text)
+    fun appendToIndex(text: String) = this.copy(index = this.index + text)
 }
