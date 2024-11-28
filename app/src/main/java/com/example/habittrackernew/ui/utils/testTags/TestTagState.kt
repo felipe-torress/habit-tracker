bc036237422bdbca5f6c0a46b3d119887616f848
Felipe Torres <fetl9239@gmail.com>
2024-11-27 21:42:07 -0300

    feat: create TestTagState

---
+++ app/src/main/java/com/example/habittrackernew/ui/utils/testTags/TestTagState.kt
@@ -0,0 +1,17 @@
+package com.example.habittrackernew.ui.utils.testTags
+
+data class TestTagState(
+    val origin: String,
+    val type: String = "",
+    val action: String = "",
+    val section: String = "",
+    val index: String = "",
+) {
+    fun type(text: String) = this.copy(type = text)
+
+    fun action(text: String) = this.copy(action = text)
+
+    fun section(text: String) = this.copy(section = text)
+
+    fun index(text: String) = this.copy(index = text)
+}
