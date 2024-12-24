8190cc4bfbf0c5bc68079f6421353a90b2aeca43
Felipe Torres <fetl9239@gmail.com>
2024-12-24 00:46:33 -0300

    fix: remove unused import

--- app/src/main/java/com/example/habittrackernew/ui/composables/colorpicker/ColorPicker.kt
+++ app/src/main/java/com/example/habittrackernew/ui/composables/colorpicker/ColorPicker.kt
@@ -3,7 +3,6 @@ package com.example.habittrackernew.ui.composables.colorpicker
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
